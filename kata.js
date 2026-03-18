a1c1a0b738ea9c840bff07a52c0860483d4117c7
Sam <samy_ch@outlook.fr>
2026-03-18 11:35:30 +0100

    🧑‍💻 Improve algo

--- kata.js
+++ kata.js
@@ -647,4 +647,17 @@ function filter_list(l) {
 	return l.filter((el) => typeof el === 'number');
 }
 
-console.log(filter_list([1, 2, 'a', 'b']));
+filter_list([1, 2, 'a', 'b']);
+
+// 7kyu return the index of the two numbers that sum to the target
+var twoSum = function (nums, target) {
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[i] + nums[j] === target) {
+				return [i, j];
+			}
+		}
+	}
+};
+
+console.log(twoSum([3, 2, 4, 3], 6));
